9f03fdc29c6ff8dd9212dc8be74ee04942c8a228
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-11-11 14:43:25 -0300

    aula 04: organizando a cofiguração com a separação dos arquivos e aliases

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ provider "aws" {
     region = "us-east-1"
 }
 
+provider "aws" {
+    alias = "us-east-2"
+    region = "us-east-2"
+}
 
 resource "aws_instance" "dev" {
     count = 3
@@ -25,27 +29,41 @@ resource "aws_instance" "dev4" {
     depends_on = [aws_s3_bucket.dev4]
 }
 
-resource "aws_security_group" "acesso-ssh" {
-  name        = "acesso-ssh"
-  description = "acesso-ssh"
-
-  ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["45.173.91.152/32"]
-  }
-
-  tags = {
-    Name = "ssh"
-  }
-}
-
 resource "aws_s3_bucket" "dev4" {
   bucket = "s3-bucket-dev-4"
   acl = "private"
 
   tags = {
     Name = "s3-bucket-dev-4"
+  }
+}
+
+resource "aws_instance" "dev5" {
+    provider = aws.us-east-2
+    ami = "ami-07693758d0ebc2111"
+    instance_type = "t2.micro"
+    key_name = "terraform-aws"
+    tags = {
+        Name = "dev-5"
+    }
+    vpc_security_group_ids = ["${aws_security_group.acesso-ssh-us-east-2.id}"]
+    depends_on = [aws_dynamodb_table.dynamodb-hml]
+}
+
+resource "aws_dynamodb_table" "dynamodb-hml" {
+  provider = aws.us-east-2
+  name           = "GameScores"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "UserId"
+  range_key      = "GameTitle"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "GameTitle"
+    type = "S"
   }
 }
---
+++ security-group.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "acesso-ssh" {
+  name        = "acesso-ssh"
+  description = "acesso-ssh"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["45.173.91.152/32"]
+  }
+
+  tags = {
+    Name = "ssh"
+  }
+}
+
+resource "aws_security_group" "acesso-ssh-us-east-2" {
+  provider = aws.us-east-2
+  name        = "acesso-ssh"
+  description = "acesso-ssh"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["45.173.91.152/32"]
+  }
+
+  tags = {
+    Name = "ssh"
+  }
+}
