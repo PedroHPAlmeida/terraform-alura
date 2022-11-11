4986b422eb79fa4d83a5e703446ca1c0ac433e06
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-11-11 13:57:32 -0300

    aula 03: utilizando referências e dependências entre os recursos

--- main.tf
+++ main.tf
@@ -11,7 +11,18 @@ resource "aws_instance" "dev" {
     tags = {
         Name = "dev-${count.index}"
     }
-    vpc_security_group_ids = ["sg-0ca6a03c9950d009c"]
+    vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
+}
+
+resource "aws_instance" "dev4" {
+    ami = "ami-0149b2da6ceec4bb0"
+    instance_type = "t2.micro"
+    key_name = "terraform-aws"
+    tags = {
+        Name = "dev-4"
+    }
+    vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
+    depends_on = [aws_s3_bucket.dev4]
 }
 
 resource "aws_security_group" "acesso-ssh" {
@@ -27,5 +38,14 @@ resource "aws_security_group" "acesso-ssh" {
 
   tags = {
     Name = "ssh"
+  }
+}
+
+resource "aws_s3_bucket" "dev4" {
+  bucket = "s3-bucket-dev-4"
+  acl = "private"
+
+  tags = {
+    Name = "s3-bucket-dev-4"
   }
 }
